b14d3debaa9cc96d6c1d4526fe9d9589d6732f77
talesnc <tales.noberto.camelo@gmail.com>
2022-06-01 20:17:13 -0300

    Creating the project

---
+++ src/main/java/com/estudo/lojatestespringboot/LojaTesteSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.lojatestespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaTesteSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaTesteSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/lojatestespringboot/LojaTesteSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.lojatestespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaTesteSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
