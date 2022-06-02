bc1d8f1004946b8c1983b6ff431f3d7a52c50c13
talesnc <tales.noberto.camelo@gmail.com>
2022-06-02 20:13:10 -0300

    Update: Resource and user entity

---
+++ src/main/java/com/estudo/lojatestespringboot/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.estudo.lojatestespringboot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudo.lojatestespringboot.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Tales", "tales@gmail.com", "999999999", "password");
+		return ResponseEntity.ok(u);
+		
+	}
+
+}
